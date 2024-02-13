8c74fc58ac120456ca3e146c39a73fc274cd8570
edmond mulera <edmondmulera55@gmail.com>
2024-02-12 22:25:21 -0800

    add

--- Books/main/views.py
+++ Books/main/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Book
+from .import serializers
 
 # Create your views here.
+class Bookviewset(viewsets.ModelViewSet):
+    queryset = Book.objects.all()
+    serializers_class = serializers.BookSerializer
