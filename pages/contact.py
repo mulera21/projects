b807c4ee1ea2cbb0028986b6993d74b07a8d29e7
edmond mulera <edmondmulera55@gmail.com>
2024-03-17 16:09:21 +0300

    complete contact form with working email

--- pages/contact.py
+++ pages/contact.py
@@ -1,10 +1,18 @@
 import streamlit as st
+from send_email import  send_email
 
 st. header("contact me")
 
 with st.form(key="my form"):
-    user_name = st.text_input("Your Email Address")
+    user_email = st.text_input("Your Email Address")
     row_message = st.text_area("Your Message")
+    message = f"""\
+Subject:new email from{user_email}
+
+From: {user_email}
+{row_message} 
+"""
     button = st.form_submit_button("submit")
     if button:
-        st.info("message sen")
+        send_email(message)
+        st.info("message send")
--- send_email.py
+++ send_email.py
@@ -1,8 +1,18 @@
 import smtplib
 import ssl
 
-host: "smtp.gmail.com"
-port : 465
 
-username = "edmondmulera55@gmail.com"
-password = "rtki hkvv phvn hylz"
+def send_email(message):
+    host = "smtp.gmail.com"
+    port = 465
+
+    username = "edmondmulera55@gmail.com"
+    password = "rtki hkvv phvn hylz"
+
+    receiver = "edmondmulera55@gmail.com"
+    context = ssl.create_default_context()
+
+    with smtplib.SMTP_SSL(host, port, context=context) as server:
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
+
